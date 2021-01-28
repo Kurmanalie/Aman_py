0932dfd4b6a4d635bbed31155691bcfe50b9091b
Aman <amankurmanaliev07@gmail.com>
2021-01-28 18:27:37 +0600

    test

---
+++ txt.py
@@ -0,0 +1,31 @@
+
+# f=open('/home/amantino/Рабочий стол/example.txt','w')
+# f.write("5674")
+
+# a = input ("Ваше имя:")
+# b = input ("Ваш пароль:")
+# c = a+b
+# f = open('/home/amantino/Рабочий стол/file.txt','w')
+# f.write(c)
+
+
+# f=open('/home/amantino/Рабочий стол/example.txt','r')
+# d="w"
+# if d in f:
+# 	print("daaa")
+# else:
+# 	print("neet")
+
+
+# a=open('/home/amantino/Рабочий стол/python.txt','r')
+# t=(a.read()).split()
+# t_words=[]
+# for x in t:
+# 	if "t" in x:
+# 		t_words.append(x)
+# 	if "T" in x:
+# 		t_words.append(x)
+# print (t_words)
+# a.close()
+
+a=open('/home/amantino/Рабочий стол/database.txt','w')
